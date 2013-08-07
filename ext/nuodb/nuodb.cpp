296261600b8aa9e9b0f57101149fc968ab5cc040
CarlosReyesStoneham <cer26@duke.edu>
2013-08-07 06:56:59 -0700

    binstr

--- ext/nuodb/nuodb.cpp
+++ ext/nuodb/nuodb.cpp
@@ -767,6 +767,7 @@ nuodb_get_rb_value(int column, SqlType type, ResultSet * results)
             }
             break;
         }
+	jump:
 	case NUOSQL_BINARY:
         case NUOSQL_VARCHAR:
         case NUOSQL_LONGVARCHAR:
@@ -815,7 +816,9 @@ nuodb_get_rb_value(int column, SqlType type, ResultSet * results)
         }
 	case 2004:
 	{
-	    printf("SUCCESS!");
+	    printf("SUCCESS!\n");
+	    type = -2;
+	    goto jump;
 	    break;
 	}
         default:
